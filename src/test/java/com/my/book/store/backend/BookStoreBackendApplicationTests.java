e0b55010fca475e934f97f61630c5db1c34fb54f
Mykola Yakymiv <mykola8426@gmail.com>
2023-08-31 22:06:41 +0300

    first commit

---
+++ src/test/java/com/my/book/store/backend/BookStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.book.store.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
